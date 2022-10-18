2af56ac9fbd5958b5bc24793809ff1642a4293f5
alexblom89 <55286258+alexblom89@users.noreply.github.com>
2022-10-18 10:35:02 -0400

    Add Hospital Model

---
+++ src/main/java/com/telemedicine/Telemedicine/Models/Hospital.java
@@ -0,0 +1,81 @@
+package com.Telemedicine.Telemedicine.Models;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table
+public class Hospital {
+    @Id
+    @SequenceGenerator(
+            name = "hp_sequence",
+            sequenceName = "hp_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "hp_sequence"
+    )
+    private Long id;
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public Hospital() {
+    }
+
+    public Hospital(Long id, String name, String address, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Hospital(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Hospital{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
