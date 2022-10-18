021d0bd16c2d1dd3208fce4fa48c584fe6a65e21
alexblom89 <55286258+alexblom89@users.noreply.github.com>
2022-10-18 10:51:31 -0400

    Add HospitalController

---
+++ src/main/java/com/telemedicine/Telemedicine/Controllers/HospitalController.java
@@ -0,0 +1,41 @@
+package com.Telemedicine.Telemedicine.Controllers;
+
+import com.Telemedicine.Telemedicine.Models.Hospital;
+import com.Telemedicine.Telemedicine.Services.HospitalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/hospital")
+public class HospitalController {
+
+    private final HospitalService hospitalService;
+
+    @Autowired
+    public HospitalController(HospitalService hospitalService) {
+        this.hospitalService = hospitalService;
+    }
+
+    @GetMapping
+    public List<Hospital> getHospitals() {
+        return hospitalService.getHospitals();
+    }
+
+    @PostMapping
+    public void registerNewHospital(@RequestBody Hospital hospital) {
+        hospitalService.addNewHospital(hospital);
+    }
+
+    @DeleteMapping(path = "{hospitalId}")
+    public void deleteHospital(@PathVariable("hospitalId") Long hospitalId) {
+        hospitalService.deleteHospital(hospitalId);
+    }
+
+    @PutMapping(path = "{hospitalId}")
+    public void updateHospital(@PathVariable("hospitalId") Long hospitalId,
+                              @RequestParam(required = false) String name) {
+        hospitalService.updateHospital(hospitalId, name);
+    }
+}
--- src/main/java/com/telemedicine/Telemedicine/Repositories/HospitalRepository.java
+++ src/main/java/com/telemedicine/Telemedicine/Repositories/HospitalRepository.java
@@ -1,10 +1,10 @@
 package com.Telemedicine.Telemedicine.Repositories;
 
-import com.Telemedicine.Telemedicine.Models.HealthcareProfessional;
+import com.Telemedicine.Telemedicine.Models.Hospital;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface HospitalRepository extends JpaRepository<HealthcareProfessional, Long> {
+public interface HospitalRepository extends JpaRepository<Hospital, Long> {
 
 }
