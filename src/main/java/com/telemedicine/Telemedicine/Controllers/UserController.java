e6160db786fe4329682a3aeb91633ed5d1bb2faa
Ryan Vickers <rdvicker@lakeheadu.ca>
2022-11-02 23:33:54 -0400

    -Changed username to email

--- src/main/java/com/telemedicine/Telemedicine/Controllers/UserController.java
+++ src/main/java/com/telemedicine/Telemedicine/Controllers/UserController.java
@@ -32,7 +32,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
-
+@CrossOrigin()
 public class UserController {
     private final UserService userService;
 
@@ -71,7 +71,7 @@ public class UserController {
                 String username = decodedJWT.getSubject();
                 User user = userService.getUser(username);
                 String access_token = JWT.create()
-                        .withSubject(user.getUsername())
+                        .withSubject(user.getEmail())
                         .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000))
                         .withIssuer(request.getRequestURL().toString())
                         .withClaim("roles", user.getRoles().stream().map(Role::getName).collect(Collectors.toList()))
--- src/main/java/com/telemedicine/Telemedicine/TelemedicineApplication.java
+++ src/main/java/com/telemedicine/Telemedicine/TelemedicineApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -18,13 +20,29 @@ public class TelemedicineApplication {
 	}
 
 	@Bean
-	CommandLineRunner commandLineRunner(UserService userService) {
-        return args -> {
-			userService.saveRole(new Role("Doctor"));
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-			userService.saveUser(new User("john","bruh","1234",new ArrayList<>()));
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
 
-			userService.addRoleToUser("bruh","Doctor");
+			userService.saveUser(new User( "John Travolta", "john", "1234", new ArrayList<>()));
+			userService.saveUser(new User("Will Smith", "will", "1234", new ArrayList<>()));
+			userService.saveUser(new User("Jim Carry", "jim", "1234", new ArrayList<>()));
+			userService.saveUser(new User("Arnold Schwarzenegger", "arnold", "1234", new ArrayList<>()));
+
+			userService.addRoleToUser("john", "ROLE_USER");
+			userService.addRoleToUser("will", "ROLE_MANAGER");
+			userService.addRoleToUser("jim", "ROLE_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_ADMIN");
+			userService.addRoleToUser("arnold", "ROLE_USER");
 		};
 		}
 
